5491251b2bab4cc2cdaf5ea119eee057b6a93fe2
Diksha Dhakate <diksha.dhakate6@gmail.com>
2020-03-26 13:01:57 +0530

    add debugger view in the application

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20200326054427) do
+ActiveRecord::Schema.define(version: 20200326065645) do
 
   create_table "articles", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
     t.string "title"
     t.text "description"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.integer "user_id"
   end
 
   create_table "users", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
